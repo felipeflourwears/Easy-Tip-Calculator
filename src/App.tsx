c28bf4c758863000c2e1bd0d47fbbeb6996b5f60
felipeflourwears <72992664+felipeflourwears@users.noreply.github.com>
2024-10-04 09:20:05 -0600

    First update Tailwind, clean environment

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+function App() {
+ 
+  return (
+    <>
+      <header className="bg-teal-400 py-5">
+        <h1 className="text-center text-4xl font-black">Easy Tip Calculator</h1>
+      </header>
+
+      
+    </>
+  )
+}
+
+export default App
